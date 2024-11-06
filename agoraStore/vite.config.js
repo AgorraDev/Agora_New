4d56b78ae9dd2d80972a086ae56a202947a809c8
agorra <agorradev@gmail.com>
2024-11-06 13:00:34 +0000

    added build to vite.config

--- agoraStore/vite.config.js
+++ agoraStore/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/', 
+  base: '/',
+  build: {
+    outDir: 'dist',
+    emptyDir: true,
+  },
   plugins: [react()],
 })
