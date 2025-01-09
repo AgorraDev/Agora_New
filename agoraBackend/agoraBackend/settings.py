15e0fd59d17d10fbcd7a49bac8983d441873df3b
agorra <agorradev@gmail.com>
2025-01-09 21:21:38 +0000

    adding cors for new branch

--- agoraBackend/agoraBackend/settings.py
+++ agoraBackend/agoraBackend/settings.py
@@ -130,6 +130,7 @@ CORS_ALLOW_ALL_ORIGINS = True #Development only!!
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',  # React development server
     'http://127.0.0.1:8000',
-    'https://agora-new-three.vercel.app/'
+    'https://agora-new-three.vercel.app/',
+    'https://agora-new-git-databaseintergration0001-agorradevs-projects.vercel.app'
     
 ]
